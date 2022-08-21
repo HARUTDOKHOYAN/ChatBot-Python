466e83f053077d0e616b6d032421b2eda49cc12a
HARUTDOKHOYAN <harut.dokhoyan46@gmail.com>
2022-08-21 18:46:18 +0400

    release V0.0.3 : -new Get File Metod , -refactoring factoris , - add Help

--- src/config.py
+++ src/config.py
@@ -1,15 +1,18 @@
 import sys
 from enum import Enum
 
-sys.path.insert(1, "./Factorys")
-sys.path.insert(1, "./Managers")
+sys.path.insert(1, "C:/Cragravorum/PYTHON/NLP/ChatBot/Factorys")
+sys.path.insert(1, "C:/Cragravorum/PYTHON/NLP/ChatBot/Managers")
 
 
-TOKEN = " "
-DayCalendarImage = " "
-WeekCalendarImage = " "
-class BotPatern (Enum):
-    FindPatern = 1
-    ListPatern = 2
-    CalendarPatern = 3 
-    GetFilePatern = 4
+TOKEN = '5581007276:AAGGEa2ZURxijMdm2uD3MPPRlBmeOv9bDrc'
+DayCalendarImage = "C:\Test.jpg"
+WeekCalendarImage = ""
+HelpText = "Hi.Тo communicate with the bot you need to use these verbs \n 'take , want , give , get , show'\n Example` \n \t \t I want List \n \t \t Give me list \n \t \t I want to give List \n  Оr write a number if iti present"
+
+
+class BotState (Enum):
+    FindState = 1
+    FindListState = 2
+    FindCalendarState = 3 
+    GetFileState = 4
