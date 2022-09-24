9acc802619c9037339b00ba49e609ac0aa32a360
HarutDokhoyan <82262019+HARUTDOKHOYAN@users.noreply.github.com>
2022-09-24 14:15:21 +0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ sys.path.insert(1, "C:/Cragravorum/PYTHON/NLP/ChatBot/Factorys")
 sys.path.insert(1, "C:/Cragravorum/PYTHON/NLP/ChatBot/Managers")
 
 
-TOKEN = '5581007276:AAGGEa2ZURxijMdm2uD3MPPRlBmeOv9bDrc'
+TOKEN = ''
 DayCalendarImage = "C:\Test.jpg"
 WeekCalendarImage = ""
 HelpText = "Hi.Тo communicate with the bot you need to use these verbs \n 'take , want , give , get , show'\n Example` \n \t \t I want List \n \t \t Give me list \n \t \t I want to give List \n  Оr write a number if iti present"
